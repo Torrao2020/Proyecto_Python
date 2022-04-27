887a9d369f3b386e31cd029cf4a6742a8152495c
Torrao2020 <omarudea@outlook.com>
2022-04-27 10:53:12 -0500

    Se agrega código Info.py V3

---
+++ Info.py
@@ -0,0 +1,17 @@
+#-------------------------------------------------------------------------------
+# Name:        module1
+# Purpose:
+#
+# Author:      Usuario
+#
+# Created:     27/04/2022
+# Copyright:   (c) Usuario 2022
+# Licence:     <your licence>
+#-------------------------------------------------------------------------------
+
+def main():
+    print("Omar Alberto Torres")
+
+
+if __name__ == '__main__':
+    main()
